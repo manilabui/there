8b1d8e57444a664ab1bba2dcd0f28624537e3137
Manila Bui <manila.bui@gmail.com>
2020-04-02 00:43:46 -0500

    resized some fonts

--- src/App.js
+++ src/App.js
@@ -71,8 +71,8 @@ export default () => {
           </div>
         </section>
       </div>
-      <footer className="absolute bottom-0 fr">
-        <p className="f7 cream mb2">Made with anguish by Manbootay | 2020</p>
+      <footer className="absolute bottom-0 o-70 fr">
+        <p className="f7 cream mb2">Made by Manchilla | 2020</p>
       </footer>
     </div>
   )
